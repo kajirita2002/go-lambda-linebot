38a02e0a8befa3c111a8333559ef84ba7ba78622
rita <kajirita2002@gmail.com>
2020-11-07 19:09:50 +0900

    [update] linebot.newのエラー処理を記述

--- linebot/go.mod
+++ linebot/go.mod
@@ -4,5 +4,5 @@ go 1.14
 
 require (
 	github.com/aws/aws-lambda-go v1.20.0
-	github.com/line/line-bot-sdk-go v7.6.0+incompatible // indirect
+	github.com/line/line-bot-sdk-go v7.6.0+incompatible
 )
--- linebot/main.go
+++ linebot/main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
+type Webhook struct {
+	Destination string           `json:"destination"`
+	Events      []*linebot.Event `json:"events"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	_, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_ACCESS_TOKEN"),
+	)
+	if err != nil {
+		log.Print(err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       fmt.Sprintf(`{"message:":"%s"}`+"\n", http.StatusText(http.StatusInternalServerError)),
+		}, nil
+	}
+	log.Print(request.Headers)
+	log.Print(request.Body)
+
+	var webhook Webhook
+
+	if err := json.Unmarshal([]byte(request.Body), &webhook); err != nil {
+		log.Print(err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", http.StatusText(http.StatusBadRequest)),
+		}, nil
+	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
